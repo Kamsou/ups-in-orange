621a40f69640311be73c121450462ad4b4836b86
Camille Coutens <camillecoutens@Camilles-MacBook-Pro.local>
2022-10-20 20:00:36 +0200

    Initial commit

---
+++ screens/CustomersScreen.tsx
@@ -0,0 +1,12 @@
+import { SafeAreaView, Text } from 'react-native'
+import React from 'react'
+
+const CustomersScreen = () => {
+  return (
+    <SafeAreaView className="justify-center items-center flex-1">
+      <Text>CustomersScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default CustomersScreen
